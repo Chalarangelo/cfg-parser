48584efba9b130962a2fe970a449de2bb4d48d6a
Angelos Chalaris <chalarangelo@gmail.com>
2025-02-22 19:14:14 +0200

    Tidy up Sym class

--- src/sym.js
+++ src/sym.js
@@ -1,31 +1,20 @@
 class Sym {
-  static #knownSymbols = new Map();
+  static #knownSymbols = new Set();
 
   static define(...names) {
     for (let name of names) {
       if (typeof name !== 'string')
         throw new TypeError('Symbol name must be a string');
 
-      const symbol = Sym.#symbolFromName(name);
-
-      if (!Sym.#knownSymbols.has(symbol)) {
-        Sym.#knownSymbols.set(symbol, name);
-        Object.defineProperty(Sym, name, { get: () => symbol });
+      if (!Sym.#knownSymbols.has(name)) {
+        Sym.#knownSymbols.add(name);
+        Object.defineProperty(Sym, name, { get: () => name });
       }
     }
   }
 
-  static get(symbol) {
-    return Sym.#knownSymbols.get(Sym.#symbolFromName(symbol));
-  }
-
   static isKnown(symbol) {
-    return Sym.#knownSymbols.has(Sym.#symbolFromName(symbol));
-  }
-
-  static #symbolFromName(name) {
-    if (typeof name === 'symbol') return name;
-    return Symbol.for(`Sym(${name})`);
+    return Sym.#knownSymbols.has(symbol);
   }
 }
 
